d7d6b195f1b626489d0a41d3aad97fdf344a261f
Dmitriy Dubson <d.dubson@gmail.com>
2016-06-04 17:17:58 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="extractd",
+    version='0.1.0',
+    packages=["extractd"],
+    license="MIT",
+    author="Dmitriy Dubson",
+    author_email="d.dubson@gmail.com",
+    install_requires=[
+        "sshtunnel"
+    ]
+)
